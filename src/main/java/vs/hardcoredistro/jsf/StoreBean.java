9ddf1667f94872553cf318a59703f6856c59fbb5
Souvatzis Vasilios <vasouv@gmail.com>
2018-07-15 00:35:27 +0300

    adds album to cart and removes

--- src/main/java/vs/hardcoredistro/jsf/StoreBean.java
+++ src/main/java/vs/hardcoredistro/jsf/StoreBean.java
@@ -17,16 +17,33 @@ public class StoreBean {
 
 	@Inject
 	private AlbumService albumService;
+	
+	@Inject
+	private CartBean cart;
 
 	private List<Album> albums;
+	
+	private Album selected;
 
 	@PostConstruct
 	public void init() {
 		albums = new ArrayList<>(albumService.findAll());
 	}
 	
+	public void addToCart() {
+		cart.add(selected);
+	}
+	
 	public List<Album> getAlbums() {
 		return albums;
+	}
+	
+	public Album getSelected() {
+		return selected;
+	}
+	
+	public void setSelected(Album selected) {
+		this.selected = selected;
 	}
 
 }
